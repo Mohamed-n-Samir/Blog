6145844d59e1663f8e6d59e83b5442cb3758a3ad
Mohamed Samir <mohamed.n.work12@gmail.com>
2026-07-03 10:48:21 +0300

    testing fastapi and jinja template (working fine)

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from fastapi-blog!")
+from fastapi import Body, FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+@app.get('/', include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html")
