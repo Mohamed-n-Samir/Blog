38a8cb81efc9537026965f62b11d972f1b6acc21
Mohamed Samir <mohamed.n.work12@gmail.com>
2026-07-03 11:17:02 +0300

    mounting static directory and test css styles

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import Body, FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
+app.mount('/static', StaticFiles(directory=BASE_DIR / 'static'), name="static")
 
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
