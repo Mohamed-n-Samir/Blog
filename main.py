2bcb3d3bbefd8a46aec1f1cca3efc4eaa0401b34
Mohamed Samir <mohamed.n.work12@gmail.com>
2026-07-05 12:24:46 +0300

    changing naming to more reasonable one, enhances error handling by adding exeption routes and adding error page to display friendly user errors

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import Body, FastAPI, Request, HTTPException, status
+from fastapi.exceptions import RequestValidationError
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
-BLOGS = [
+POSTS = [
     {
         "id": 1,
         "slug": "almost-half-of-ai-written-code-fails-in-production",
@@ -57,18 +60,23 @@ app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 
-# Client
+# Client pages
 @app.get("/", include_in_schema=False, name="root")
-@app.get("/blogs", include_in_schema=False, name="blogs")
-def home(request: Request):
-    return templates.TemplateResponse(
-        request, "pages/index.html", {"blogs": BLOGS, "blogsLen": len(BLOGS)}
-    )
-
-
 @app.get("/blog", include_in_schema=False, name="blog")
-def blog(request: Request):
-    return templates.TemplateResponse(request, "pages/blog.html", {"blog": BLOGS[1]})
+def home(request: Request):
+    return templates.TemplateResponse(request, "pages/index.html", {"posts": POSTS})
+
+
+@app.get("/blog/post/{id}", include_in_schema=False, name="post")
+def post(id: int, request: Request):
+    for post in POSTS:
+        if post["id"] == id:
+            return templates.TemplateResponse(
+                request, "pages/post.html", {"post": post}
+            )
+    return templates.TemplateResponse(
+        request, "errors/404.html",context={"message": "Post Not found"}, status_code=status.HTTP_404_NOT_FOUND
+    )
 
 
 @app.get("/categories", include_in_schema=False, name="categories")
@@ -76,7 +84,72 @@ def categories(request: Request):
     return templates.TemplateResponse(request, "pages/categories.html")
 
 
-# Server
+# Server API
+@app.get("/api/blog/post/{id}", include_in_schema=True)
+def get_post(id: int, request: Request):
+    for post in POSTS:
+        if post["id"] == id:
+            return {"post": post}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="there is no post exist with that id",
+            )
 
 
+# Exceptions
+@app.exception_handler(status.HTTP_404_NOT_FOUND)
+def not_found(request: Request, exc: HTTPException):
+    message = "The page you're looking for doesn't exist or has been moved."
+    
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"detail": message},
+        )
 
+    return templates.TemplateResponse(
+        request=request,
+        name="errors/404.html",
+        context={"message": message},
+        status_code=status.HTTP_404_NOT_FOUND,
+    )
+
+
+@app.exception_handler(StarletteHTTPException)
+def handle_exceptions(request: Request, exc: StarletteHTTPException):
+    message = (
+        exc.detail
+        if exc.detail
+        else "An error occurred. Please check your request and try again."
+    )
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"detail": message},
+        )
+
+    return templates.TemplateResponse(
+        request=request,
+        name="errors/error.html",
+        context={"message": message, "status_code": exc.status_code},
+        status_code=exc.status_code,
+    )
+
+
+@app.exception_handler(RequestValidationError)
+def handle_validation_errors(request: Request, exc: RequestValidationError):
+    if request.url.path.startswith('/api'):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            content={"detail": exc.errors()},
+        )
+    
+    error_msg = "Invalid request. Please check your input and try again."
+    
+    return templates.TemplateResponse(
+        request=request,
+        name="errors/error.html",
+        context={"message": error_msg, "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT},
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+    )
