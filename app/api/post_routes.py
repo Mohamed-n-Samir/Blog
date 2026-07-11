e468222f2f7bb5c86a98c8bfc9e6fd37990397c9
Mohamed Samir <mohamed.n.work12@gmail.com>
2026-07-11 14:01:11 +0300

    post can accept tags and new tags

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -10,6 +10,9 @@ from app.models.schemas import PostPostResponse, PostCreate
 
 from app.services.user_service import UserService
 from app.services.post_service import PostService
+from app.services.tag_service import TagService
+
+from app.models.models import Tag
 
 from app.utils.exceptions import NotFoundException
 
@@ -48,15 +51,30 @@ async def create_post(post: PostCreate, tags: list[str], db: DBSession):
 
     new_post = Post(**(post.model_dump()))
 
+
+    # Remove duplicates
+    tag_names = list(set(tags))
+
+    tag_service = TagService(db)
+
+    existing_tags = await tag_service.get_tags_by_names(tags)
+    existing_by_name = {tag.name: tag for tag in existing_tags}
+
+    new_tags = [
+        Tag(name=name)
+        for name in tag_names
+        if name not in existing_by_name
+    ]
+
+    tag_service.add_all(new_tags)
+
     post_service = PostService(db)
 
-    # for tag in tags:
+    new_post.tags.extend(existing_tags)
+    new_post.tags.extend(new_tags)
 
 
     saved_post = await post_service.add(new_post)
-    # post_author = await saved_post.awaitable_attrs.author
-
-    # print(f"Post created by {post_author.name}")
         
     return saved_post
 
--- app/services/tag_service.py
+++ app/services/tag_service.py
@@ -1,6 +1,11 @@
-from sqlalchemy import or_, and_
+from collections.abc import Sequence
+from typing import Any, TypeVar
+
+from sqlalchemy import ColumnElement, or_, and_
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import InstrumentedAttribute
+from sqlalchemy.orm.interfaces import ExecutableOption
 
 from app.repositories.tag_repository import TagRepository
 
@@ -12,18 +17,33 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+T = TypeVar("T")
+
+
 class TagService:
     def __init__(self, db: AsyncSession):
         self.repo = TagRepository(db)
 
     async def get_by_name(self, name: str):
         return await self.repo.get_by(name=name)
-    
+
     async def get_all(self):
         return await self.repo.get_all()
+
+    async def get_tags_by_names(
+            self, 
+            tag_names: list[str], 
+            options: Sequence[ExecutableOption] = ()
+        ) -> Sequence[Tag]:
+            if not tag_names:
+                return []
+                
+            return await self.repo.find_all(
+                Tag.name.in_(tag_names),
+                options=options
+            )
     
     async def add(self, tag: Tag):
-
         try:
             tag = await self.repo.add(tag)
             await self.repo.db.commit()
@@ -32,7 +52,7 @@ class TagService:
             await self.repo.db.rollback()
             logger.error("Error while trying to save the new tag", e)
             raise ConflictException(f"Tag already exists, {e._message}")
-    
+
     async def add_all(self, tags: list[Tag]):
         try:
             tags = await self.repo.add_all(tags)
